b7f5010fa3742ddfb5b998eeb9f30d848bc7f20e
gustavopurkoot <gustavopurkoot04053357@gmail.com>
2025-11-18 18:21:12 -0300

    Refactor damage calculation and add STAB
    Updated damage calculation formula to Gen 1 standards and added STAB bonus.

--- TrabalhoPokemon/Pokemon.c
+++ TrabalhoPokemon/Pokemon.c
@@ -5,7 +5,7 @@ char pokemons[3][20] = {"Charmander", "Squirtle", "Bulbasaur"};
 int hp[2][3] = {{39,44,45}, {39,44,45}};
 int ataque[2][3] = {{52,48,49}, {52,48,49}};
 int defesa[2][3] = {{43,65,49}, {43,65,49}};
-int tipos[2][3] = {{1,2,3}, {1,2,3}};
+int tipos[2][3] = {{1,2,3}, {1,2,3}}; // 1=fogo, 2=água, 3=planta
 
 char ataques[3][4][20] = {
     {"Arranhar","Brasa","Lanca-Chamas","Investida"},
@@ -18,25 +18,37 @@ int tipo_ataque[3][4] = {{0,1,1,0}, {0,2,2,0}, {3,3,0,0}};
 int ativo[2] = {0, 0};
 
 
+// ======= FÓRMULA DE DANO GEN 1 ========
 int calcular_dano(int atacante, int defensor, int ataque_escolhido) {
-    int nivel = 50;
-    int poder_ataque = poder[ativo[atacante]][ataque_escolhido];
-    int atk = ataque[atacante][ativo[atacante]];
-    int def = defesa[defensor][ativo[defensor]];
-    
+    double nivel = 50.0;
 
-    int dano = ((2 * nivel + 10) * poder_ataque * atk / def / 250) + 2;
-    
+    double poder_ataque = poder[ativo[atacante]][ataque_escolhido];
+    double atk = (double) ataque[atacante][ativo[atacante]];
+    double def = (double) defesa[defensor][ativo[defensor]];
 
+    // Fórmula Gen 1:
+    double dano = (((((2 * nivel) / 5) + 2) * poder_ataque * (atk / def)) / 50) + 2;
+
+    // STAB (Same Type Attack Bonus)
+    if (tipo_ataque[ativo[atacante]][ataque_escolhido] == tipos[atacante][ativo[atacante]]) {
+        dano *= 1.5;
+    }
+
+    // Super efetivo
     if((tipo_ataque[ativo[atacante]][ataque_escolhido] == 1 && tipos[defensor][ativo[defensor]] == 3) ||
        (tipo_ataque[ativo[atacante]][ataque_escolhido] == 3 && tipos[defensor][ativo[defensor]] == 2) ||
-       (tipo_ataque[ativo[atacante]][ataque_escolhido] == 2 && tipos[defensor][ativo[defensor]] == 1)) {
-        dano = (dano * 3) / 2;
+       (tipo_ataque[ativo[atacante]][ataque_escolhido] == 2 && tipos[defensor][ativo[defensor]] == 1)) 
+    {
+        dano *= 2.0;
         printf("SUPER EFETIVO! ");
     }
-    
-    return (dano < 1) ? 1 : dano;
+
+    int dano_final = (int)dano;
+    if(dano_final < 1) dano_final = 1;
+
+    return dano_final;
 }
+
 
 int tem_pokemon_vivo(int jogador) {
     for(int i = 0; i < 3; i++) {
@@ -107,7 +119,6 @@ int main() {
     printf("Jogador 2: "); scanf("%s", jogadores[1]);
     
     printf("\n--- BATALHA POKEMON ---\n");
-    
 
     escolher_pokemon(0);
     escolher_pokemon(1);
@@ -125,7 +136,7 @@ int main() {
     }
     
     printf("\n=== VITORIA ===\n");
-    printf("🎉 %s venceu!\n", tem_pokemon_vivo(0) ? jogadores[0] : jogadores[1]);
+    printf("%s venceu!\n", tem_pokemon_vivo(0) ? jogadores[0] : jogadores[1]);
     
     return 0;
 }
